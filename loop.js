7972acba90d698a6010ca70aa2c26c572281bbc3
GringoGrigg <equidza@gmail.com>
2026-02-17 11:37:14 +0300

    for each loop

---
+++ loop.js
@@ -0,0 +1,5 @@
+//Make an array of your friends' names. Loop through it and alert() a greeting to each.
+const names = ['okise','ekada','ekisa',"ong'alla",'ajakaa','okwara','ikwara'];
+const greeting = names.forEach(function(name){
+alert("Hello" + name);
+});
